85d358db53d1c7a9a9f264aecf3cabf965194e65
Tino <tinodafunk@googlemail.com>
2020-08-04 12:14:04 +0100

    Metronome Lab

---
+++ src/Components/PlayControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlayControls = (props) => {
+  return(
+    <div>
+  <button onClick={props.playBeep} >Click to beep</button>
+  </div>
+)
+}
+
+
+
+export default PlayControls;
